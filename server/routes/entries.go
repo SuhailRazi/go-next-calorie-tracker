465f61a64e209c0113011bc311a41cda0d041ecc
suhail <suhail.r@hubspire.com>
2024-06-15 21:21:54 +0530

    front end and create entry implemented

--- server/routes/entries.go
+++ server/routes/entries.go
@@ -14,7 +14,28 @@ import (
 
 var entryCollection *mongo.Collection = openCollection(Client, "calories")
 
-func AddEntry(c *gin.Context) {}
+func AddEntry(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var entries []bson.M
+	cursor, err := entryCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	if err = cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
+}
 
 func GetEntries(c *gin.Context) {}
 
